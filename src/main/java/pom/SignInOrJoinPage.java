099bd50fc750683b83dee94f8febc565f409ff17
akashmorey <moreya750@.com>
2023-09-13 14:59:26 +0530

    update code

--- src/main/java/pom/SignInOrJoinPage.java
+++ src/main/java/pom/SignInOrJoinPage.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SignInOrJoinPage {
-	@FindBy (xpath = "//input[@id='verify-account-email']") private WebElement emailFied;
-	@FindBy (xpath = "//div[text()='Email address cannot be blank.']") private WebElement emailFieldError;
-	@FindBy (xpath = "(//button[@type='submit'])[2]") private WebElement continewButton;
-	@FindBy (xpath = "//a[text()='Manage']") private WebElement manageLink;
-	@FindBy (xpath = "//button[text()='Back']") private WebElement backButton;
+	@FindBy (xpath = "//input[@id='verify-account-email']") 			private WebElement emailFied;
+	@FindBy (xpath = "//div[text()='Email address cannot be blank.']") 	private WebElement emailFieldError;
+	@FindBy (xpath = "(//button[@type='submit'])[2]") 					private WebElement continewButton;
+	@FindBy (xpath = "//a[text()='Manage']") 							private WebElement manageLink;
+	@FindBy (xpath = "//button[text()='Back']") 						private WebElement backButton;
 	
 	public SignInOrJoinPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
