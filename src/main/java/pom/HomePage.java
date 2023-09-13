caaa4e8860556b7c1a9360e368560b6bba6be7d3
akashmorey <moreya750@.com>
2023-09-13 16:29:10 +0530

    update Pom classes

--- src/main/java/pom/HomePage.java
+++ src/main/java/pom/HomePage.java
@@ -19,7 +19,7 @@ public class HomePage {
 	@FindBy (xpath = "//input[@name='email']")					private WebElement emailField;
 	@FindBy (xpath = "//input[@name='phoneNumber']")			private WebElement mobileNumberField;
 	@FindBy (xpath = "//button[text()='Sign Up for Emails']")	private WebElement signUpForEmails;
-	@FindBy (xpath = "//button[text()='Sign up for Texts']")	private WebElement signUpForMobilr;
+	@FindBy (xpath = "//button[text()='Sign up for Texts']")	private WebElement signUpForMobile;
 	@FindBy (xpath = "//a[text()='SIGN IN OR JOIN']")			private WebElement signUpButtonLink;
 	@FindBy (xpath = "//button[@data-testid='account-button']")	private WebElement accountButton;
 	@FindBy (xpath = "//button[@id='shopping-bag']")			private WebElement shoppingBagbutton;
@@ -29,5 +29,66 @@ public class HomePage {
 
 	public HomePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickOnNewButtonLink() {
+		newButtonLink.click();
+	}
+	public void clickOnWomenButtonLink() {
+		womenButtonLink.click();
+	}
+	public void clickOnMenButtonLink() {
+		menButtonLink.click();
+	}
+	public void clickOnbabyAndToddlerButtonLink () {
+		babyAndToddlerButtonLink.click();
+	}
+	public void clickOnHomeButtonLink () {
+		homeButtonLink.click();
+	}
+	public void clickOnBrBasicsButtonLink () {
+		BrBasicsButtonLink.click();
+	}
+	public void clickOnCashmereButtonLink () {
+		cashmereButtonLink.click();
+	}
+	public void clickOnSaleButtonLink () {
+		saleButtonLink.click();
+	}
+	public void enterSearchField(String product) {
+		searchField.sendKeys(product);
+	}
+	public void clickOnSearchButton () {
+		searchButton.click();
+	}
+	public void enterEmailField(String email) {
+		emailField.sendKeys(email);
+	}
+	public void enterMobileNumberField(String mob) {
+		mobileNumberField.sendKeys(mob);
+	}
+	public void clickOnSignUpForEmails () {
+		signUpForEmails.click();
+	}
+	public void clickOnSignUpForMobile () {
+		signUpForMobile.click();
+	}
+	public void clickOnSignUpButtonLink () {
+		signUpButtonLink.click();
+	}
+	public void clickOnAccountButton () {
+		accountButton.click();
+	}
+	public void clickOnShoppingBagbutton () {
+		shoppingBagbutton.click();
+	}
+	public void clickOnFindStoreButtonLink () {
+		findStoreButtonLink.click();
+	}
+	public void clickOnGiftCardButtonLink () {
+		giftCardButtonLink.click();
+	}
+	public void clickOnCreaditCardButtonLink() {
+		creaditCardButtonLink.click();
 	}
 }
--- src/main/java/pom/SignInOrJoinPage.java
+++ src/main/java/pom/SignInOrJoinPage.java
@@ -9,11 +9,27 @@ public class SignInOrJoinPage {
 	@FindBy (xpath = "//input[@id='verify-account-email']") 			private WebElement emailFied;
 	@FindBy (xpath = "//div[text()='Email address cannot be blank.']") 	private WebElement emailFieldError;
 	@FindBy (xpath = "(//button[@type='submit'])[2]") 					private WebElement continewButton;
-	@FindBy (xpath = "//a[text()='Manage']") 							private WebElement manageLink;
+	@FindBy (xpath = "//a[text()='Manage']") 	 						private WebElement manageLink; 
 	@FindBy (xpath = "//button[text()='Back']") 						private WebElement backButton;
 	
 	public SignInOrJoinPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
-
+	
+	public void enterEmailField(String email) {
+		emailFied.sendKeys(email);
+	}
+	public boolean verifyEmailErrorMassege() {
+		boolean result=emailFieldError.isDisplayed();
+		return result;
+	}
+	public void clickOnContinewButton() {
+		continewButton.click();
+	}
+	public void clickOnManageButtonLink() {
+		manageLink.click();
+	}
+	public void clickOnBackButton() {
+		backButton.click();
+	}
 }
