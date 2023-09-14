7c662942c5fa5ca465b71dd329979ea950804445
NandkumarKamble1997 <nandkumarkamble358@gmail.com>
2023-09-15 00:47:43 +0530

    listener class added

--- src/main/java/utility/Listener.java
+++ src/main/java/utility/Listener.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class Listener extends BaseTest implements ITestListener {
+public class Listener extends BaseTest implements ITestListener{
+	
 	
 	public void onTestStart(ITestResult result)
 	{
